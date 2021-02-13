a9e9b0ac365f0ef6fae58acea9d202644f702cb1
Yuri Caetano <yuuri.caetano@gmail.com>
2021-02-13 14:00:41 -0300

    boiler plate with, styled components, story book, pwa, jest, eslint and prettier

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,12 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Main from '.'
+
+export default {
+  title: 'Main',
+  component: Main,
+  argTypes: {
+    backgroudColor: { control: 'color' }
+  }
+} as Meta
+
+export const Basic: Story = (args) => <Main {...args} />
